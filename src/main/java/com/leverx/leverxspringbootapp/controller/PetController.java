771de6d634fd3936d8e6ee409ce1695153265058
andrei.tatarynau <1259489Kill>
2021-06-23 10:12:53 +0300

    Code refactoring

--- src/main/java/com/leverx/leverxspringbootapp/controller/PetController.java
+++ src/main/java/com/leverx/leverxspringbootapp/controller/PetController.java
@@ -4,7 +4,11 @@ import com.leverx.leverxspringbootapp.entity.Pet;
 import com.leverx.leverxspringbootapp.service.PetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -13,23 +17,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PetController {
 
+    private final static String PET_PATH_ID = "/{id}";
+    private final static String PET_PATH_GET_ALL = "/all";
+
     private final PetService petService;
 
-    @GetMapping("/{id}")
+    @GetMapping(PET_PATH_ID)
     public ResponseEntity<Pet> getById(@PathVariable("id") Long id){
 
         Pet pet = petService.getById(id);
         return ResponseEntity.ok(pet);
     }
 
-    @GetMapping("/all")
+    @GetMapping(PET_PATH_GET_ALL)
     public ResponseEntity<List<Pet>> getAll(){
 
         List<Pet> pets = petService.getAllPets();
         return ResponseEntity.ok(pets);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping(PET_PATH_ID)
     public ResponseEntity<List<Pet>> deleteById(@PathVariable("id") Long id){
 
         petService.delete(id);
