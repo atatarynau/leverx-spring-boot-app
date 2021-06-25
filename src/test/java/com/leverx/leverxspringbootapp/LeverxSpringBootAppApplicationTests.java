0e6732b58713378318917b685fe27ecbfe69bb8c
andrei.tatarynau <1259489Kill>
2021-06-25 16:45:00 +0300

    Changing log style

--- src/test/java/com/leverx/leverxspringbootapp/LeverxSpringBootAppApplicationTests.java
+++ src/test/java/com/leverx/leverxspringbootapp/LeverxSpringBootAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.leverx.leverxspringbootapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = "prod")
 class LeverxSpringBootAppApplicationTests {
 
     @Test
